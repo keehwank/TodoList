644fb00fbe5fe302b6cb1dd5ef041d4172ad299c
keehwank <48819214+keehwank@users.noreply.github.com>
2019-06-27 20:58:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import "./index.css";
 // import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
-import TodoApp from "./components/todoapp";
+import TodoApp from "./components/TodoApp";
 
 ReactDOM.render(<TodoApp />, document.getElementById("root"));
 
